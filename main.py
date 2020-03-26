9a97cd4532e90bfa6bba8bb7c1b614d6ea835c6f
subhayuroy <subhayu.roy2001@gmail.com>
2020-03-27 01:50:07 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+import requests
+
+def scrape():
+    response = requests.get(URL+COIN)
+    response_json = response.json()
+    return float(response_json[0]['price_usd'])
+
+URL = 'https://pro.coinmarkrtcap.com/migrate/'
+COIN = "bitcoin"
+last_price = None
+
+while True:
+    latest_price = scrape()
+    if latest_price != last_price:
+        print("Latest Price: ",latest_price)
+        last_price = latest_price
